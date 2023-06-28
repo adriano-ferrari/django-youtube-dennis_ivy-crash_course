9f044400a6228b15e55b9690617d19821bc7f52a
adriano-ferrari <irarrefonairda@gmail.com>
2023-06-28 08:20:28 -0300

    Aula 06 - Database Relationships | One To One & Many To Many

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Customer(models.Model):
 	name = models.CharField(max_length=200, null=True)
@@ -12,11 +11,12 @@ class Customer(models.Model):
 		return self.name
 
 
-#class Tag(models.Model):
-#	name = models.CharField(max_length=200, null=True)
-#
-#	def __str__(self):
-#		return self.name
+class Tag(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.name
+
 
 class Product(models.Model):
 	CATEGORY = (
@@ -29,10 +29,11 @@ class Product(models.Model):
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
 	description = models.CharField(max_length=200, null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
-#	tags = models.ManyToManyField(Tag)
+	tags = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return self.name
+
 
 class Order(models.Model):
 	STATUS = (
